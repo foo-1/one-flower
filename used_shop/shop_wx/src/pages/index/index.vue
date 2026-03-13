05eb5fe1aa57f14cca88301d96f8367d4d64bb8c
foo-1 <2402332026@qq.com>
2026-03-13 15:03:28 +0800

    test
    截图测试

---
+++ used_shop/shop_wx/src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="content">
+	  
+	  <u-button type="primary">主要按钮</u-button>
+	  <u-button type="success">成功按钮</u-button>
+	  
+    <image class="logo" src="/static/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
